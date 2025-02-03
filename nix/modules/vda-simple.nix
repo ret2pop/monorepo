66c78fdb55211cc071b87761635284f17b7c7c4c
Preston Pan <ret2pop@gmail.com>
2025-02-03 15:41:35 -0800

    add some files

---
+++ nix/modules/vda-simple.nix
@@ -0,0 +1,35 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/vda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              priority = 1;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              priority = 2;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

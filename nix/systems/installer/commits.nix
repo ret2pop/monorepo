c9bd87d8dd7369ea4163f0dbf4d0d28948726a36
Preston Pan <ret2pop@gmail.com>
2025-09-04 15:54:34 -0700

    pin installer to prev

--- nix/systems/installer/commits.nix
+++ nix/systems/installer/commits.nix
@@ -1,4 +1,4 @@
 {
-  diskoCommitHash = "latest";
-  monorepoCommitHash = "HEAD";
+  diskoCommitHash = "a5c4f2ab72e3d1ab43e3e65aa421c6f2bd2e12a1";
+  monorepoCommitHash = "ba22dd89c67162aa26c7f85c389314ef1f079ace";
 }

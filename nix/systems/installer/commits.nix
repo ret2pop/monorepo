32c47bb286e5c39a7d32aebbcfc82b855b503e8d
Preston Pan <ret2pop@gmail.com>
2025-09-09 02:29:25 -0700

    update installer to new configuration

--- nix/systems/installer/commits.nix
+++ nix/systems/installer/commits.nix
@@ -1,5 +1,5 @@
 {
-  diskoCommitHash = "a5c4f2ab72e3d1ab43e3e65aa421c6f2bd2e12a1";
-  monorepoCommitHash = "5b8d09f2d7ebb7a1670c695af5761353d5b76d7e";
+  diskoCommitHash = "c8a0e78d86b12ea67be6ed0f7cae7f9bfabae75a";
+  monorepoCommitHash = "64a1acc961466c845b2601c0d0e165b16eb73fe2";
   monorepoUrl = "https://github.com/ret2pop/monorepo";
 }

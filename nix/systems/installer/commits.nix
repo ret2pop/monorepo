a4a80065fead83b89f79543719efcc3983781cee
Preston Pan <ret2pop@gmail.com>
2025-09-20 00:50:19 -0700

    modify monorepo commit hash installer

--- nix/systems/installer/commits.nix
+++ nix/systems/installer/commits.nix
@@ -1,5 +1,5 @@
 {
   diskoCommitHash = "c8a0e78d86b12ea67be6ed0f7cae7f9bfabae75a";
-  monorepoCommitHash = "e2fbfac7ad7c0b4d438179b31d2ed53d884313a0";
+  monorepoCommitHash = "070a98e56367a8414d6844438f5cc091cc10effb";
   monorepoUrl = "https://github.com/ret2pop/monorepo";
 }

c0c2a56d27d26681650a7216aae92a8928377238
Preston Pan <ret2pop@gmail.com>
2026-01-30 20:56:13 -0800

    fix org file

--- nix/systems/spontaneity/default.nix
+++ nix/systems/spontaneity/default.nix
@@ -1,92 +1,82 @@
-lib, ... }:
-    let
-      ipv4addr = "66.42.84.130";
-      ipv6addr = "2001:19f0:5401:10d0:5400:5ff:fe4a:7794";
-    in
-    {
-      imports = [
-        ../common.nix
-        ../../disko/drive-bios.nix
+{ lib, ... }:
+  let
+    ipv4addr = "66.42.84.130";
+    ipv6addr = "2001:19f0:5401:10d0:5400:5ff:fe4a:7794";
+  in
+  {
+    imports = [
+      ../common.nix
+      ../../disko/drive-bios.nix
 
-        # nixos-anywhere generates this file
-        ./hardware-configuration.nix
-      ];
-      config = {
-        monorepo = {
-          vars.device = "/dev/vda";
-          profiles = {
-            server.enable = true;
-            ttyonly.enable = true;
-            grub.enable = true;
-            pipewire.enable = false;
-            tor.enable = false;
-            home.enable = false;
-          };
+      # nixos-anywhere generates this file
+      ./hardware-configuration.nix
+    ];
+    config = {
+      monorepo = {
+        vars.device = "/dev/vda";
+        profiles = {
+          server.enable = true;
+          ttyonly.enable = true;
+          grub.enable = true;
+          pipewire.enable = false;
+          tor.enable = false;
+          home.enable = false;
         };
+      };
 
-        boot.loader.grub.device = "nodev";
-        networking = {
-          interfaces.ens3.ipv4.addresses = [
-            {
-              address = ipv4addr;
-              prefixLength = 24;
-            }
-          ];
-          interfaces.ens3.ipv6.addresses = [
-            {
-              address = ipv6addr;
-              prefixLength = 64;
-            }
-          ];
-          firewall.allowedTCPPorts = [
-            80
-            143
-            443
-            465
-            587
-            993
-            6697
-            6667
-            8448
-          ];
-          domains = {
-            enable = true;
-            baseDomains = {
-              "${config.monorepo.vars.remoteHost}" = {
-                a.data = ipv4addr;
-                aaaa.data = ipv6addr;
-              };
-              "${config.monorepo.vars.orgHost}" = {
-                a.data = ipv4addr;
-                aaaa.data = ipv6addr;
-              };
+      boot.loader.grub.device = "nodev";
+      networking = {
+        interfaces.ens3.ipv4.addresses = [
+          {
+            address = ipv4addr;
+            prefixLength = 24;
+          }
+        ];
+        interfaces.ens3.ipv6.addresses = [
+          {
+            address = ipv6addr;
+            prefixLength = 64;
+          }
+        ];
+        firewall.allowedTCPPorts = [
+          80
+          143
+          443
+          465
+          587
+          993
+          6697
+          6667
+          8448
+        ];
+        domains = {
+          enable = true;
+          baseDomains = {
+            "${config.monorepo.vars.remoteHost}" = {
+              a.data = ipv4addr;
+              aaaa.data = ipv6addr;
             };
-            subDomains = {
-              "${config.monorepo.vars.remoteHost}" = {};
-              "matrix.${config.monorepo.vars.remoteHost}" = {};
-              "www.${config.monorepo.vars.remoteHost}" = {};
-              "mail.${config.monorepo.vars.remoteHost}" = {
-                mx.data = "10 mail.${config.monorepo.vars.remoteHost}.";
-              };
+            "${config.monorepo.vars.orgHost}" = {
+              a.data = ipv4addr;
+              aaaa.data = ipv6addr;
+            };
+          };
+          subDomains = {
+            "${config.monorepo.vars.remoteHost}" = {};
+            "matrix.${config.monorepo.vars.remoteHost}" = {};
+            "www.${config.monorepo.vars.remoteHost}" = {};
+            "mail.${config.monorepo.vars.remoteHost}" = {
+              mx.data = "10 mail.${config.monorepo.vars.remoteHost}.";
+            };
 
-              "${config.monorepo.vars.orgHost}" = {};
-              "git.${config.monorepo.vars.orgHost}" = {};
-              "matrix.${config.monorepo.vars.orgHost}" = {};
-              "talk.${config.monorepo.vars.orgHost}" = {};
-              "mail.${config.monorepo.vars.orgHost}" = {};
-              "${config.monorepo.vars.internetName}.${config.monorepo.vars.orgHost}" = {};
-            };
+            "${config.monorepo.vars.orgHost}" = {};
+            "git.${config.monorepo.vars.orgHost}" = {};
+            "matrix.${config.monorepo.vars.orgHost}" = {};
+            "talk.${config.monorepo.vars.orgHost}" = {};
+            "mail.${config.monorepo.vars.orgHost}" = {};
+            "${config.monorepo.vars.internetName}.${config.monorepo.vars.orgHost}" = {};
           };
         };
       };
-    }
-  #+end_src
-  **
-#+begin_src nix :tangle ../nix/systems/spontaneity/home.nix
-  { lib, config, pkgs, ... }:
-  {
-    imports = [
-      ../home-common.nix
-    ];
-    config.monorepo.profiles.enable = false;
+    };
   }
